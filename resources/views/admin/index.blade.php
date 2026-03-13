7bb8f1378a735251dea834124d347971e2fe6f4f
abinash2055 <abinashnathpandey@gmail.com>
2026-03-13 13:11:37 +0545

    admin integrated

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,21 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
+<!-- head  -->
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    @include('admin.css')
 </head>
+<!-- head  -->
 
 <body>
-    <x-app-layout>
-        <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Admin Dashboard') }}
-            </h2>
-        </x-slot>
-    </x-app-layout>
+    <!-- Header  -->
+    @include('admin.header')
+    <!-- Header  -->
 
+    <!-- Sidebar Navigation-->
+    @include('admin.sidebar')
+    <!-- Sidebar Navigation end-->
+
+    <!-- body section  -->
+    @include('admin.body')
+    <!-- body section  -->
+
+    <!-- Footer  -->
+    @include('admin.footer')
+    <!-- Footer  -->
 </body>
 
 </html>
--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -32,11 +32,25 @@
                                 <a class="nav-link" href="gallery.html">Gallery</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="blog.html">Blog</a>
-                            </li>
-                            <li class="nav-item">
                                 <a class="nav-link" href="contact.html">Contact Us</a>
                             </li>
+
+                            @if (Route::has('login'))
+                                @auth
+                                    <x-app-layout>
+                                    </x-app-layout>
+                                @else
+                                    <li class="nav-item">
+                                        <a class="btn btn-success" style="margin-right:10px;" href="{{url('login')}}">Login</a>
+                                    </li>
+
+                                    @if (Route::has('register'))
+                                        <li class="nav-item">
+                                            <a class="btn btn-primary" href="{{url('register')}}">Register</a>
+                                        </li>
+                                    @endif
+                                @endauth
+                            @endif
                         </ul>
                     </div>
                 </nav>
